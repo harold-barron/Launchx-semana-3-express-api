7d0e20ce3727c2dcb8480a5d036527dd9619f190
Harold Barron <haroldbarron13@gmail.com>
2022-04-26 20:41:11 -0500

    Updating app.js

--- express_api/app.js
+++ express_api/app.js
@@ -5,7 +5,7 @@ const express = require('express')
 const app = express()
 
 //indicamos que se va a utilizar un archivo json
- app.use(express.json)
+ app.use(express.json())
 
 //indicamos el puerto
 const port = 3000
