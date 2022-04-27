a0241e91b3ce43bd50df84e277380a1a51975ac2
Harold Barron <haroldbarron13@gmail.com>
2022-04-26 20:26:55 -0500

    initializing the app

--- express_api/app.js
+++ express_api/app.js
@@ -10,3 +10,7 @@ const app = express()
 //indicamos el puerto
 const port = 3000
 
+// Con esto inicializamos esta app
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+   })
